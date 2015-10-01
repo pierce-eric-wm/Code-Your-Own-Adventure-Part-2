6a79155063a5af4f85e9d345fff00f9d90046db6
Eric <eric-pierce@west-mec.org>
2015-10-01 13:41:19 -0700

    This is the start of my project.

---
+++ README.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by session1 on 10/1/15.
+ */
+Read me plz.
+
+
+    This is my game. You can win but you will also lose. Every choice that you make will effect your outcome.
+    GOOD LUCK.
