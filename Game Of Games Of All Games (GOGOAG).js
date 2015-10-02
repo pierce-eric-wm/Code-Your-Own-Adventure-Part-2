2aa9e1c1b811d3ceeebc5e8b1606b1793e24609e
Eric <eric-pierce@west-mec.org>
2015-10-02 14:55:57 -0700

    this adds more to the game

--- Game Of Games Of All Games (GOGOAG).js
+++ Game Of Games Of All Games (GOGOAG).js
@@ -5,37 +5,75 @@
         "in a blink of an eye the plane hit the water knocking you out cold... You awoke after 5 hours. You are on a island some where in the altntic ocean. There is no body in sight " +
         "and you start to panic. After you calm down you make the decison to do something. You need to survive for as long as you can.");
 
-    var start = prompt ("You need to get ready for the night. Do you build a shelter, look for food, or do you get some water.");
-    if (start === "shelter");// this is the first choice that the player makes (1) ---◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊---
+    var start = prompt ("You need to get ready for the night. Do you build a shelter, look for food, or do you get some water. (shelter/food/water)");
+    if (start === "shelter") // this is the first choice that the player makes (1) ---◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊--- shelter > ?
     {
         alert("You get up and start to grap some branches off a fallen palm tree. After an hour you have a shelter built and will protect you from the rain.");
-        var choice2 = prompt("Now that you have a shelter what do you do for the rest of the day? Do you get water or look for food? (You see some dark clouds in the distance)");
-        if (choice2 === "water")
-            {
+        var choice2 = prompt("Now that you have a shelter what do you do for the rest of the day? Do you get water or look for food? (You see some dark clouds in the distance)  (water/food)");
+        if (choice2 === "water") // shelter > water
+        {
             alert("You look for a container to hold the water and a fliter to filter the water. Luckily there is a suit case that has a water bottle and a filter for water" +
                 "you spend the rest of the day getting water.");
             alert("night time");
-            }
-        else if (choice2 === "food") {
+        }
+        else if (choice2 === "food") // shelter > food
+        {
             alert("You look around and find a bag. The bag has some food for you to eat. You spend the rest of the day getting food. You also find a container.");
             alert("night time")
-            }
-        else if (start === "water")
+        }
+    }
+        else if (start === "water") // water > ?
             { //this is the 2nd chioic the player can make (2) ---◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊---
             alert("You look for a container to hold the water and a fliter to filter the water. Luckily there is a suit case that has a water bottle and a filter for water");
-            var choice3 = prompt("Now that you have water you need to do something quick before night falls. Do you look for food or build a shelter?");
-            if (choice3 === "food")
+            var choice3 = prompt("Now that you have water you need to do something quick before night falls. Do you look for food or build a shelter? (food/shelter)" );
+            if (choice3 === "food") // water > food
             {
                 alert("You look around and find a bag. The bag has some food for you to eat. You spend the rest of the day getting food. You also find a container.");
                 alert("night time");
             }
-            else if (choise3 === "shelter")
+            else if (choice3 === "shelter") // water > shelter
             {
                 alert("You get up and start to grap some branches off a fallen palm tree. After an hour you have a shelter built and will protect you from the rain.");
                 alert("night time");
+                var nightWaterShelter = prompt  ("It started to rain now and since you already have water you can't get any more in the container. You built a shelter so you don't get cold and wet" +
+                    " You hear a noise do you check it out or do you" +
+                    "stay put. (check/stay)");
             }
-            }
+            }// end of the 2nd choice (maybe) i might need to make this one and the others go on because of the choice they make. or find a way to get them to link to each other.
+     // this is the end of the if start and var start.
+    else if (start = "food") { // food > ?
+        alert ("You look around and find a bag. The bag has some food for you to eat. You spend half of the day getting food.You also find a container.");
+        var choice4 = prompt ("You still have half of the day left. Do you build a shelter or do you get water. (shelter/water)");
+        if (choice4 === "shelter") // food > shelter
+         {
+            alert("You get up and start to grap some branches off a fallen palm tree. After an hour you have a shelter built and will protect you from the rain.");
+            alert("night time");
+             var nightFoodShelter = prompt ("The clouds roll over your shelter and it starts to rain. You get your container and it fills up. You " +
+                 "didn't need a filter because it is rain water. You hear a noise do you check it out or do you stay put? (check/stay)");
+        }
+        else if (choice4 === "water")// food > water
+        {
+            alert("You have a container for the water so now you look for a filter. You find a filter in a bag that just washed up. The gods must be watching you.");
+            alert("night time");
+                            var nightFoodWater = prompt ("It started to rain now and since you already have water you can't get any more in the container. You are soaked because " +
+                "you didn't build a shelter. This makes you really cold over the night and you get sick. You hear a noise do you check it out or do you" +
+                "stay put. (check/stay)");
+
+
+        }
     }
 
+
+/*
+I need to make the game go off of the branches so now i will have to make a whole new branch in all of the branches
+
+            ^
+           ^ ^
+          ^ ^ ^
+         ^ ^ ^ ^                <---  Will look like this not really but it is close to it.
+        ^ ^ ^ ^ ^
+       ^ ^ ^ ^ ^ ^
+      ^ ^ ^ ^ ^ ^ ^ 
+ */
 
 
