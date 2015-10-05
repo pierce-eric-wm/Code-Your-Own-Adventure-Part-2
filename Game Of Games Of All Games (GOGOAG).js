2a959e98d523b7acbad28531f9d702f07b8a686f
Eric <eric-pierce@west-mec.org>
2015-10-05 14:31:18 -0700

    Add more to the game + switch statements

--- Game Of Games Of All Games (GOGOAG).js
+++ Game Of Games Of All Games (GOGOAG).js
@@ -1,5 +1,5 @@
 
-    alert ("Press (OK) when you are ready to play the game.");
+    alert ("Press (OK) when you are ready to play the game. (not a finished game but i needed to turn it in)");
     alert ("(Story) It felt like a normal day for you but something was a little off. You left your wife and kids for an important buissness trip in England." +
         "You got on the plane and after 10 min. of waiting the plane finaly took off. 1 and a half hours in the plane got hit by lightning and the plane is coming down fast." +
         "in a blink of an eye the plane hit the water knocking you out cold... You awoke after 5 hours. You are on a island some where in the altntic ocean. There is no body in sight " +
@@ -15,12 +15,12 @@
             alert("You look for a container to hold the water and a fliter to filter the water. Luckily there is a suit case that has a water bottle and a filter for water" +
                 "you spend the rest of the day getting water.");
             alert("night time");
+
         }
         else if (choice2 === "food") // shelter > food
         {
             alert("You look around and find a bag. The bag has some food for you to eat. You spend the rest of the day getting food. You also find a container.");
             alert("night time")
-        }
     }
         else if (start === "water") // water > ?
             { //this is the 2nd chioic the player can make (2) ---◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊◊---
@@ -58,22 +58,61 @@
                             var nightFoodWater = prompt ("It started to rain now and since you already have water you can't get any more in the container. You are soaked because " +
                 "you didn't build a shelter. This makes you really cold over the night and you get sick. You hear a noise do you check it out or do you" +
                 "stay put. (check/stay)");
+            if (nightFoodWater === "check") {
+                alert("You go and check out what the noise is and as you approch you see something dart across your view. It was fast. You go scared and " +
+                    "ran back to the hut because you have no weapon to defend yourself.")
+            }
+                else if (nightFoodWater === 'stay') {
+                alert ('You made the best decision to stay and wait out the night. You were a little curious but you decided to stay. Do you want to try and change the out come?')
 
-
+            }
         }
+    }
+    alert ('A bear comes out and attacks you! during the night time.')
+
+    var fight = prompt ("How do you want to fight the bear? (hints: stick,fists, run, pie...")
+
+    switch(fight){
+        case 'stick':
+            console.log("You try to fight the bear off with the stick but you lose. GAME OVER.");
+            break;
+        case 'fists':
+            console.log("You tryed to fight the bear with your hands but its claws worked better. GAME OVER");
+            break;
+        case 'run':
+            console.log("You try to outrun the bear but the bear is part of a evil experiment gone wrong causeing it to run as fast as a cheeta, the bear catches up. GAME OVER.");
+            break;
+        case 'pie':
+            console.log("You some how make a pie and you give the pie to the bear and the bear gives you a plane and you fly out of the island... YOU WIN!");
+            break;
+        default:
+            console.log("You sit and wonder how to beat the bear but you though for too long the bear eats you. YOU LOSE! GAME OVER!");
     }
 
 
-/*
-I need to make the game go off of the branches so now i will have to make a whole new branch in all of the branches
 
-            ^
-           ^ ^
-          ^ ^ ^
-         ^ ^ ^ ^                <---  Will look like this not really but it is close to it.
-        ^ ^ ^ ^ ^
-       ^ ^ ^ ^ ^ ^
-      ^ ^ ^ ^ ^ ^ ^ 
- */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
